ac7cc96fad03408d6a91ae0062bffda7e12cc5ef
kengos <kengo@kengos.jp>
2023-08-29 22:20:54 +0900

    p311

---
+++ src/ch4/trait_TreasureBox.rs
@@ -0,0 +1,57 @@
+trait TreasureBox {
+    fn open(&self, key_no: i32) -> bool;
+    fn check(&self);
+}
+
+struct JewlryBox {
+    price: i32,
+    key_no: i32,
+}
+
+impl TreasureBox for JewlryBox {
+    fn open(&self, key_no: i32) -> bool {
+        self.key_no == key_no
+    }
+
+    fn check(&self) {
+        println!("宝石箱だった！金貨{}枚入手", self.price);
+    }
+}
+
+struct TrapBox {
+    damage: i32,
+}
+
+impl TreasureBox for TrapBox {
+    fn open(&self, _: i32) -> bool {
+        return true; // どんな鍵でも開く
+    }
+
+    fn check(&self) {
+        println!("罠だった！{}のダメージ", self.damage);
+    }
+}
+
+fn open_box(tbox: &impl TreasureBox, key_no: i32) {
+    if !tbox.open(key_no) {
+        println!("鍵が合わず宝箱が開きません。");
+        return;
+    }
+    tbox.check();
+}
+
+fn main() {
+    let box1 = JewlryBox {
+        price: 30,
+        key_no: 1,
+    };
+    let box2 = TrapBox { damage: 3 };
+    let box3 = JewlryBox {
+        price: 20,
+        key_no: 2,
+    };
+    let my_key = 2;
+    open_box(&box1, my_key);
+    open_box(&box2, my_key);
+    open_box(&box3, my_key);
+}
